37776efd2df7f8443099cb6c240a08fac1abaca9
vaibhav2007sharma-byte <vaibhav2007sharma@gmail.com>
2026-02-19 17:26:23 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@ import React from "react";
 function App() {
   return (
     <div style={{ padding: 20 }}>
-      <h1>Vidya Institute CRM</h1>
-      <h2>Dashboard</h2>
+      <h1>Vidya Institute CRM Dashboard</h1>
+      <h2>Welcome Vaibhav</h2>
 
-      <div>
+      <div style={{ marginTop: 20 }}>
         <p>Total Students: 1</p>
         <p>Total Fees Collected: ₹2000</p>
+        <p>Total Inquiry: 5</p>
       </div>
     </div>
   );
