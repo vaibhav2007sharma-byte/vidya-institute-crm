210d04d29552758ecf8f05c28abd03e313f7c41d
vaibhav2007sharma-byte <vaibhav2007sharma@gmail.com>
2026-02-19 16:41:24 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Vidya Institute CRM</h1>
+      <h2>Dashboard</h2>
+
+      <div>
+        <p>Total Students: 1</p>
+        <p>Total Fees Collected: ₹2000</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
