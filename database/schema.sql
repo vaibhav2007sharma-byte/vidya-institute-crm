bf64bf5052f81815b71c1c6559e570648c18c49d
vaibhav2007sharma-byte <vaibhav2007sharma@gmail.com>
2026-02-19 16:42:06 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,13 @@
 CREATE TABLE students (
-id SERIAL PRIMARY KEY,
-name VARCHAR(100)
+ id SERIAL PRIMARY KEY,
+ name VARCHAR(100),
+ mobile VARCHAR(20),
+ course VARCHAR(100),
+ fee INT
+);
+
+CREATE TABLE fees (
+ id SERIAL PRIMARY KEY,
+ student_id INT,
+ amount INT
 );
